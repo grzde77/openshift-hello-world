19387b524426e19c01dc92272bb756583fb3df60
grzde77 <155643159+grzde77@users.noreply.github.com>
2025-01-03 16:12:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World in WWW - Deployed on OpenShift Sandbox! -ver6"
+    return "Hello World in WWW - Deployed on OpenShift Sandbox! -ver7"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
